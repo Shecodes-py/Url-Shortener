d803898544d1c9b55d2ca6c0f0c4f742b7d31b10
Ella Udotong <udotongpeace@gmail.com>
2025-09-16 20:01:37 +0100

    Changed port to match

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const moongoose = require('mongoose');
 
 const app = express();
-const port = 8000;
+const port = 5000;
 
 app.use(express.json()) // middleware-parsing
 
